890d9ff60ad98f9ab9c3cb88bffb4e4728d17acd
Victorya01 <76602663+Victorya01@users.noreply.github.com>
2021-02-25 01:33:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ггг
+{
+    
+             class Program
+        {
+
+            static bool Binary(ref int[] arr, int key)
+            {
+
+                int L = 0, R = arr.Length - 1, mid;
+
+                while (L <= R)
+                {
+                    mid = (R + L) / 2;
+                    if (arr[mid] == key) { return true; }
+                    else if (arr[mid] < key) { L = mid + 1; }
+                    else if (arr[mid] > key) { R = mid - 1; }
+                }
+                return false;
+            }
+
+            static void Main(string[] args)
+            {
+
+                int size;
+                size = int.Parse(Console.ReadLine());
+
+
+
+                int[] arr = new int[size];
+                for (int i = 0; i < size; ++i)
+                {
+                    arr[i] = i + 1;
+                }
+                Console.WriteLine(Binary(ref arr, 140));
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+    
+
